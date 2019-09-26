7e59c1ce24fe41c85bab63462bfeac92c20c6507
Yingqi Jing <350956009@qq.com>
2019-09-26 17:21:12 +0200

    Note to use relative path in the bash script
    Note to use relative path rather than absolute path in the bash script for output. Otherwise, gc3pie will not be able to retrieve the results!

--- convert.sh
+++ convert.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-# Note: you need to have imagemagick installed (sudo apt install imagemagick). 
+# Note: 1. you need to have imagemagick installed (sudo apt install imagemagick). 
+# 2. the output file or folder should be a relative path, instead of an absolute path. Otherwise, gc3pie will likely not be able to retrieve the output!
+
 IMG=$1
 convert /home/ubuntu/tutorial_gc3pie/in-data/$IMG -colorspace gray gray-${IMG}
+
